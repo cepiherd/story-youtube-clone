c2d0557dc8aec388d80c95d0ab32126c62662c03
cepiherd <cepiherd@gmail.com>
2022-04-22 15:03:59 +0700

    :hammer: updating dynamic data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Short from "./lib/components/short/short";
 
 function App() {
+  let storyData = [{ id:1 , channel  :"Cepiherd" , caption: 'Clone youtube as you like' , channelImage: "https://avatars.githubusercontent.com/u/52832395?v=4" , urlYoutube : "https://www.youtube.com/embed/yOeNo6aTizg?autoplay=0&controls=0"} , {id: 2, channel : "Hena Nurohmah" , caption : 'Cloning again', channelImage: "https://pbs.twimg.com/profile_images/939332634164969474/YTld9EH9_400x400.jpg",urlYoutube : "https://www.youtube.com/embed/dEOhEEElRNA?autoplay=0&controls=0"}]
+  const StoryEach = storyData.map((item) => {
+   return  (<Short channel={item.channel} caption={item.caption} channelImage={item.channelImage} urlYoutube={item.urlYoutube}/>)
+  })
   return (
     <main className=" flex justify-center w-full h-screen">
       <div className="  h-full  md:px-2 snap-mandatory snap-y overflow-y-auto  ">
-        <Short />
-        <Short />
-        <Short />
+        {StoryEach}
       </div>
     </main>
   );
--- src/lib/components/short/short.jsx
+++ src/lib/components/short/short.jsx
@@ -1,13 +1,28 @@
 import { Icon } from "@iconify/react";
-function Short() {
+import { useEffect, useState } from "react";
+function Short(props) {
+  const [IsShowToggle, setIsShowToggle] = useState(false);
+  const [CapChoose, setCapChoose] = useState("");
+  const handleToogle = (toogle, caption) => {
+    setIsShowToggle(toogle);
+    setCapChoose(caption);
+  };
   return (
     <div className="relative md:flex my-5 first:mt-10 last:mb-40">
       <div className="  w-[371px] rounded-2xl h-[661px] bg-slate-700  snap-center relative shadow">
+        <object
+          className="w-[371px] h-[661px] float-none clear-both rounded-2xl"
+          data={props.urlYoutube}
+        ></object>
         <div className=" absolute bottom-0 text-white w-full">
+          <div className="px-2 text-white">{props.caption}</div>
           <div className="px-2 py-[16px] flex justify-between w-full">
-            <div class="self-center flex w-full">
-              <span className="p-[18px] bg-slate-300 rounded-full mr-2"></span>
-              <span className="self-center">Cepi Herdiansyah</span>
+            <div className="self-center flex w-full">
+              <span
+                className="p-[18px] bg-slate-300 rounded-full mr-2 bg-contain"
+                style={{ backgroundImage: `url(${props.channelImage})` }}
+              ></span>
+              <span className="self-center">{props.channel}</span>
             </div>
             <button
               name="Subscribe for loving it"
@@ -21,7 +36,7 @@ function Short() {
       <div className=" absolute md:relative  right-0 bottom-20 md:top-0 h-[661px]  flex flex-col px-2  justify-end md:justify-between md:pl-2   md:w-[76px] text-white">
         <div className="self-center">
           <div className="flex justify-center pt-10">
-            <div className="block md:hidden">
+            <div className="block md:hidden cursor-pointer">
               <Icon
                 icon="carbon:overflow-menu-horizontal"
                 color="#aaa"
@@ -29,7 +44,7 @@ function Short() {
                 height="24"
               />
             </div>
-            <div className="hidden md:block">
+            <div className="hidden md:block cursor-pointer">
               <Icon
                 icon="carbon:overflow-menu-vertical"
                 width="24"
@@ -39,32 +54,56 @@ function Short() {
             </div>
           </div>
         </div>
-        <div class="flex flex-col">
-          <div className="my-3 self-center">
-            <div className="flex justify-center">
+        <div className="flex flex-col">
+          <div className="my-3 self-center relative">
+            <div
+              className="flex justify-center cursor-pointer"
+              onMouseEnter={() => {
+                handleToogle(true, "Like");
+              }}
+              onMouseLeave={() => setIsShowToggle(false)}
+            >
               <Icon
                 icon="fa6-solid:thumbs-up"
                 width="24"
                 height="24"
                 color="#AAAAAA"
               />
+              <Toggle
+                IsShowToggle={IsShowToggle}
+                caption={"Like"}
+                capChoose={CapChoose}
+              />
             </div>
             <span>21K</span>
           </div>
           <div className="my-3 self-center">
-            <div className="flex justify-center">
+            <div
+              className="flex justify-center cursor-pointer relative"
+              onMouseEnter={() => handleToogle(true, "Dislike")}
+              onMouseLeave={() => setIsShowToggle(false)}
+            >
               <Icon
                 icon="fa6-solid:thumbs-down"
                 color="#aaa"
                 width="24"
                 height="24"
               />
+              <Toggle
+                IsShowToggle={IsShowToggle}
+                caption="Dislike"
+                capChoose={CapChoose}
+              />
             </div>
             <span>Dislike</span>
           </div>
 
           <div className="my-3 self-center">
-            <div className="flex justify-center">
+            <div
+              className="flex justify-center cursor-pointer relative"
+              onMouseEnter={() => handleToogle(true, "Comment")}
+              onMouseLeave={() => setIsShowToggle(false)}
+            >
               <Icon
                 icon="ic:round-message"
                 color="#aaa"
@@ -72,12 +111,26 @@ function Short() {
                 height="24"
                 hFlip={true}
               />
+              <Toggle
+                IsShowToggle={IsShowToggle}
+                caption="Comment"
+                capChoose={CapChoose}
+              />
             </div>
             <span>220</span>
           </div>
           <div className="my-3 self-center text-center">
-            <div className="flex justify-center">
+            <div
+              className="flex justify-center cursor-pointer relative"
+              onMouseEnter={() => handleToogle(true, "Share")}
+              onMouseLeave={() => setIsShowToggle(false)}
+            >
               <Icon icon="fa-solid:share" color="#aaa" width="24" height="24" />
+              <Toggle
+                IsShowToggle={IsShowToggle}
+                caption="Share"
+                capChoose={CapChoose}
+              />
             </div>
 
             <span>Share</span>
@@ -89,3 +142,13 @@ function Short() {
 }
 
 export default Short;
+
+function Toggle(props) {
+  if (props.IsShowToggle === true && props.capChoose === props.caption) {
+    return (
+      <div className="absolute top-0 right-10  backdrop-opacity-60 bg-slate-400/30 font-normal px-2 py-1 rounded backdrop-invert ">
+        {props.caption}
+      </div>
+    );
+  }
+}
